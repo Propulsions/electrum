997c2f317eea5bcd1db283e3de920ccd6e19a9d6
Propulsion <Propulsion@DarkcoinTalk.org>
2015-02-18 14:29:23 -0500

    Darkcoin: testnet values for lib/bitcoin.py

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -239,7 +239,7 @@ def public_key_to_bc_address(public_key):
     h160 = hash_160(public_key)
     return hash_160_to_bc_address(h160)
 
-def hash_160_to_bc_address(h160, addrtype = 204): #change to Darkcoin private key type. 
+def hash_160_to_bc_address(h160, addrtype = 239): #Darkcoin TESTNET private key type. Darkcoin/src/chainparams.cpp L139
     vh160 = chr(addrtype) + h160
     h = Hash(vh160)
     addr = vh160 + h[0:4]
@@ -324,12 +324,12 @@ def PrivKeyToSecret(privkey):
     return privkey[9:9+32]
 
 
-def SecretToASecret(secret, compressed=False, addrtype=204): #darkcoin
+def SecretToASecret(secret, compressed=False, addrtype= 239): #Darkcoin TESTNET private key type. Darkcoin/src/chainparams.cpp L139
     vchIn = chr((addrtype+128)&255) + secret
     if compressed: vchIn += '\01'
     return EncodeBase58Check(vchIn)
 
-def ASecretToSecret(key, addrtype=204): #darkcoin
+def ASecretToSecret(key, addrtype= 239): #Darkcoin TESTNET private key type. Darkcoin/src/chainparams.cpp L139
     vch = DecodeBase58Check(key)
     if vch and vch[0] == chr((addrtype+128)&255):
         return vch[1:]
